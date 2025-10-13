bcd677d7a187c90a37896bab1fb17cea736e8e4f
lokeshvaishnav11 <lokeshkumariit@gmail.com>
2025-10-13 11:51:05 +0530

    GHJ

--- socket/index.ts
+++ socket/index.ts
@@ -183,7 +183,7 @@ io.on("connection", (socket: Socket) => {
 
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 server.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
